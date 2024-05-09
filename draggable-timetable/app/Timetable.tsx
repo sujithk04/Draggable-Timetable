ad2a393723ecc55b3ddf9a15ce61576afc7b4472
Xinlyu Wang <xinlalalele@gmail.com>
2024-05-09 18:56:26 +0800

    Version 3

--- draggable-timetable/app/Timetable.tsx
+++ draggable-timetable/app/Timetable.tsx
@@ -19,6 +19,70 @@ type Schedule = {
   [key: string]: ClassItem | undefined;
 };
 
+type TimeTableRowsProps = {
+  times: string[];
+  hoveredTime: string | null;
+  hoveredColor: string | null;
+  days: string[];
+  schedule: Schedule;
+};
+
+const TimeTableRows: React.FC<TimeTableRowsProps> = ({
+  times,
+  hoveredTime,
+  hoveredColor,
+  days,
+  schedule,
+}) => {
+  return (
+    <>
+      {times.map((time) => (
+        <tr key={time}>
+          <th
+            className={`px-4 py-2 ${
+              hoveredTime === time ? hoveredColor : "text"
+            }`}
+          >
+            {time}
+          </th>
+          {days.map((day) => (
+            <Droppable key={day} droppableId={`${day}-${time}`}>
+              {(provided) => (
+                <td
+                  ref={provided.innerRef}
+                  {...provided.droppableProps}
+                  className="border px-4 py-2 list-none"
+                >
+                  {schedule[`${day}-${time}`] && (
+                    <Draggable
+                      draggableId={schedule[`${day}-${time}`]?.id!}
+                      index={0}
+                    >
+                      {(provided) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          className={`p-2 mb-2 text ${
+                            schedule[`${day}-${time}`]?.color.split(" ")[0]
+                          } text-white rounded`}
+                        >
+                          {schedule[`${day}-${time}`]?.name}
+                        </div>
+                      )}
+                    </Draggable>
+                  )}
+                  {provided.placeholder}
+                </td>
+              )}
+            </Droppable>
+          ))}
+        </tr>
+      ))}
+    </>
+  );
+};
+
 const initialClasses: ClassItem[] = [
   { id: "1", name: "Math", color: "bg-blue-500 text-blue-500" },
   { id: "2", name: "English", color: "bg-green-500 text-green-500" },
@@ -32,10 +96,10 @@ const initialClasses: ClassItem[] = [
 
 const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 const timesMorning = ["9AM", "10AM", "11AM"];
+
 const timesAfternoon = ["1PM", "2PM"];
 
-const Timetable = () => {
-  const [classes, setClasses] = useState<ClassItem[]>(initialClasses);
+function Timetable() {
   const [schedule, setSchedule] = useState<Schedule>({});
   const [hoveredDay, setHoveredDay] = useState<string | null>(null);
   const [hoveredTime, setHoveredTime] = useState<string | null>(null);
@@ -52,7 +116,7 @@ const Timetable = () => {
     const destinationId = destination.droppableId;
 
     if (sourceId === "classes" && destinationId !== "classes") {
-      const movedClass = classes[source.index];
+      const movedClass = initialClasses[source.index];
       const newClassInstance = { ...movedClass, id: generateUniqueId() };
 
       setSchedule((prev) => ({
@@ -78,7 +142,6 @@ const Timetable = () => {
       });
     }
 
-    // 拖拽结束时，清空 hovered day, time 和 color
     setHoveredDay(null);
     setHoveredTime(null);
     setHoveredColor(null);
@@ -86,245 +149,117 @@ const Timetable = () => {
 
   const onDragUpdate = (update: DragUpdate) => {
     const sourceId = update.source.droppableId;
-    if (sourceId === "classes") {
-      const sourceIndex = update.source.index;
-      const hoveredClass = classes[sourceIndex];
-      setHoveredColor(hoveredClass.color.split(" ")[1]);
 
-      const destinationId = update.destination?.droppableId;
-      if (destinationId) {
-        const [day, time] = destinationId.split("-");
-        setHoveredDay(day);
-        setHoveredTime(time);
-      }
+    const sourceIndex = update.source.index;
+    const hoveredClass = initialClasses[sourceIndex];
+    setHoveredColor(hoveredClass.color.split(" ")[1]);
+
+    const destinationId = update.destination?.droppableId;
+    if (destinationId) {
+      const [day, time] = destinationId.split("-");
+      setHoveredDay(day);
+      setHoveredTime(time);
     }
-  };
-
-  type TimeTableRowsProps = {
-    times: string[];
-  };
-
-  const TimeTableRows: React.FC<TimeTableRowsProps> = ({ times }) => {
-    return (
-      <>
-        {times.map((time) => (
-          <tr key={time}>
-            <th
-              className={`px-4 py-2 ${
-                hoveredTime === time ? hoveredColor : ""
-              }`}
-            >
-              {time}
-            </th>
-            {days.map((day) => (
-              <Droppable key={day} droppableId={`${day}-${time}`}>
-                {(props) => (
-                  <td
-                    ref={props.innerRef}
-                    {...props.droppableProps}
-                    className="border px-4 py-2 list-none"
-                  >
-                    {schedule[`${day}-${time}`] && (
-                      <Draggable
-                        draggableId={schedule[`${day}-${time}`]?.id!}
-                        index={0}
-                      >
-                        {(props) => (
-                          <div
-                            ref={props.innerRef}
-                            {...props.draggableProps}
-                            {...props.dragHandleProps}
-                            className={`p-2 mb-2 ${
-                              schedule[`${day}-${time}`]?.color.split(" ")[0]
-                            } text-white rounded`}
-                          >
-                            {schedule[`${day}-${time}`]?.name}
-                          </div>
-                        )}
-                      </Draggable>
-                    )}
-                    {props.placeholder}
-                  </td>
-                )}
-              </Droppable>
-            ))}
-          </tr>
-        ))}
-      </>
-    );
   };
 
   return (
     <DragDropContext onDragEnd={onDragEnd} onDragUpdate={onDragUpdate}>
-      <div className="grid grid-cols-1 md:grid-cols-5 gap-4 p-4">
-        <Droppable droppableId="classes">
-          {(props) => (
-            <div
-              className="col-span-1 bg-white shadow-lg p-4 rounded-md"
-              {...props.droppableProps}
-              ref={props.innerRef}
-            >
-              <h2 className="text-xl font-bold mb-4">Available Classes</h2>
-              <ul className="list-none">
-                {classes.map((cls, index) => (
-                  <Draggable
-                    key={cls.id}
-                    draggableId={cls.id.toString()}
-                    index={index}
-                  >
-                    {(props) => (
-                      <li
-                        ref={props.innerRef}
-                        {...props.draggableProps}
-                        {...props.dragHandleProps}
-                        className={`p-2 mb-2 ${
-                          cls.color.split(" ")[0]
-                        } text-white rounded`}
-                      >
-                        {cls.name}
-                      </li>
-                    )}
-                  </Draggable>
-                ))}
-                {props.placeholder}
-              </ul>
-            </div>
-          )}
-        </Droppable>
-
-        <div className="md:col-span-4 bg-white shadow-lg p-4 rounded-md">
-          <h2 className="text-xl font-bold mb-4">Curriculum Schedule</h2>
-          <table className="table-auto w-full">
-            <thead>
-              <tr>
-                <th className="px-4 py-2"></th>
-                {days.map((day) => (
-                  <th
-                    key={day}
-                    className={`px-4 py-2 ${
-                      hoveredDay === day ? hoveredColor : ""
-                    }`}
-                  >
-                    {day}
-                  </th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-              {timesMorning.map((time) => (
-                <tr key={time}>
-                  <th
-                    className={`px-4 py-2 ${
-                      hoveredTime === time ? hoveredColor : ""
-                    }`}
-                  >
-                    {time}
-                  </th>
-                  {days.map((day) => (
-                    <Droppable key={day} droppableId={`${day}-${time}`}>
+      <div className="h-full bg-gray-100 dark:bg-gray-800">
+        <div className="grid grid-cols-1 md:grid-cols-5 gap-4 p-4 shadow-lg">
+          <Droppable droppableId="classes">
+            {(props) => (
+              <div
+                {...props.droppableProps}
+                ref={props.innerRef}
+                className="col-span-1 bg-white dark:bg-gray-700 shadow-lg p-4 rounded-md"
+              >
+                <h2 className="text-xl font-bold mb-4 text">
+                  Available Classes
+                </h2>
+                <ul className="non-list">
+                  {initialClasses.map((cls, index) => (
+                    <Draggable
+                      key={cls.id}
+                      draggableId={cls.id.toString()}
+                      index={index}
+                    >
                       {(props) => (
-                        <td
+                        <li
                           ref={props.innerRef}
-                          {...props.droppableProps}
-                          className="border px-4 py-2 list-none"
+                          {...props.draggableProps}
+                          {...props.dragHandleProps}
+                          className={`p-2 mb-2 ${
+                            cls.color.split(" ")[0]
+                          } text-white rounded`}
                         >
-                          {schedule[`${day}-${time}`] && (
-                            <Draggable
-                              draggableId={schedule[`${day}-${time}`]?.id!}
-                              index={0}
-                            >
-                              {(props) => (
-                                <div
-                                  ref={props.innerRef}
-                                  {...props.draggableProps}
-                                  {...props.dragHandleProps}
-                                  className={`p-2 mb-2 ${
-                                    schedule[`${day}-${time}`]?.color.split(
-                                      " "
-                                    )[0]
-                                  } text-white rounded`}
-                                >
-                                  {schedule[`${day}-${time}`]?.name}
-                                </div>
-                              )}
-                            </Draggable>
-                          )}
-                          {props.placeholder}
-                        </td>
+                          {cls.name}
+                        </li>
                       )}
-                    </Droppable>
+                    </Draggable>
+                  ))}
+                  {props.placeholder}
+                </ul>
+              </div>
+            )}
+          </Droppable>
+          <div className="md:col-span-4 bg-white dark:bg-gray-700 shadow-lg p-4 rounded-md">
+            <h2 className="text-xl font-bold mb-4 text">Curriculum Schedule</h2>
+            <table className="table-auto w-full">
+              <thead>
+                <tr>
+                  <th className="px-4 py-2"></th>
+                  {days.map((day) => (
+                    <th
+                      key={day}
+                      className={`px-4 py-2 ${
+                        hoveredDay === day ? hoveredColor : "text"
+                      }`}
+                    >
+                      {day}
+                    </th>
                   ))}
                 </tr>
-              ))}
-              <tr>
-                <td className="px-4 py-2" />
-                <td
-                  colSpan={days.length}
-                  className="text-center font-bold border px-4 py-2"
-                >
-                  Noon Break
-                </td>
-              </tr>
-              {timesAfternoon.map((time) => (
-                <tr key={time}>
-                  <th
-                    className={`px-4 py-2 ${
-                      hoveredTime === time ? hoveredColor : ""
-                    }`}
+              </thead>
+              <tbody>
+                <TimeTableRows
+                  times={timesMorning}
+                  hoveredTime={hoveredTime}
+                  hoveredColor={hoveredColor}
+                  days={days}
+                  schedule={schedule}
+                />
+                <tr>
+                  <td className="px-4 py-2" />
+                  <td
+                    colSpan={days.length}
+                    className="text-center font-bold border border-gray-300 px-4 py-2 text"
                   >
-                    {time}
-                  </th>
-                  {days.map((day) => (
-                    <Droppable key={day} droppableId={`${day}-${time}`}>
-                      {(props) => (
-                        <td
-                          ref={props.innerRef}
-                          {...props.droppableProps}
-                          className="border px-4 py-2 list-none"
-                        >
-                          {schedule[`${day}-${time}`] && (
-                            <Draggable
-                              draggableId={schedule[`${day}-${time}`]?.id!}
-                              index={0}
-                            >
-                              {(props) => (
-                                <div
-                                  ref={props.innerRef}
-                                  {...props.draggableProps}
-                                  {...props.dragHandleProps}
-                                  className={`p-2 mb-2 ${
-                                    schedule[`${day}-${time}`]?.color.split(
-                                      " "
-                                    )[0]
-                                  } text-white rounded`}
-                                >
-                                  {schedule[`${day}-${time}`]?.name}
-                                </div>
-                              )}
-                            </Draggable>
-                          )}
-                          {props.placeholder}
-                        </td>
-                      )}
-                    </Droppable>
-                  ))}
+                    Noon Break
+                  </td>
                 </tr>
-              ))}
-              <tr>
-                <td className="px-4 py-2" />
-                <td
-                  colSpan={days.length}
-                  className="text-center font-bold border px-4 py-2"
-                >
-                  Extra Curricular
-                </td>
-              </tr>
-            </tbody>
-          </table>
+                <TimeTableRows
+                  times={timesAfternoon}
+                  hoveredTime={hoveredTime}
+                  hoveredColor={hoveredColor}
+                  days={days}
+                  schedule={schedule}
+                />
+                <tr>
+                  <td className="px-4 py-2" />
+                  <td
+                    colSpan={days.length}
+                    className="text-center font-bold border border-gray-300 px-4 py-2 text"
+                  >
+                    Extra Curricular
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
         </div>
       </div>
     </DragDropContext>
   );
-};
+}
 
 export default Timetable;
